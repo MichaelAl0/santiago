49b628738c0a6b3189e360b137c5adea9942677f
MichaelAI0 <alphasni@mail.com>
2022-08-12 23:38:30 -0500

    tailwind css

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="">
+      <h1>This is the Header</h1>
+      <Image />
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
+    </div>
+  );
 }
